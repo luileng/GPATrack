cc402e404605e9250ff93cffa96b49df1276b87b
luileng <chengluileng@gmail.com>
2025-03-09 10:10:52 +0800

    Change courses primary key to course_code

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE students (
 );
 
 CREATE TABLE courses (
-    course_id SERIAL PRIMARY KEY,
+    course_code VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
@@ -31,11 +31,11 @@ CREATE TABLE grade_scale (
 CREATE TABLE grades (
     student_id INT NOT NULL,
     semester_id INT NOT NULL,
-    course_id INT NOT NULL,
+    course_code VARCHAR(10) NOT NULL,
     grade CHAR(2) NOT NULL,
     PRIMARY KEY (student_id, semester_id),
     FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
-    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_code) REFERENCES courses(course_code) ON DELETE CASCADE,
     FOREIGN KEY (semester_id) REFERENCES semesters(semester_id) ON DELETE CASCADE,
     FOREIGN KEY (grade) REFERENCES grade_scale(grade) ON DELETE CASCADE
 );
